07bafbc055cac740af0d8e60d78c5d7f54f6782d
chiaralle <chiaraallegrino@gmail.com>
2025-07-03 20:03:27 +0200

    homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./components/HomePage";
+
 function App() {
 
   return (
     <>
-      
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
+    </BrowserRouter>
     </>
   )
 }
